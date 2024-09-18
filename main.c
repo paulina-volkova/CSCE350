afd4db8a014360239393e147e115ea576f8f8b63
Ubuntu <ubuntu@ip-172-31-8-123.us-east-2.compute.internal>
2024-09-18 17:04:54 +0000

    Lab02 all files

---
+++ main.c
@@ -0,0 +1,13 @@
+extern long long int test();
+extern long long int lab02b();
+extern void lab02c(long long int a);
+extern long long int lab02d(long long int a);
+
+int main(void)
+{
+	test();
+	lab02b();
+	lab02c(250);
+	lab02d(250);
+    	return 0;
+}
